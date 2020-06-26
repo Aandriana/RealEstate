f8a47284319d40e39dc699f647813a9e9f9757b0
Aandriana <abaran@leobit.com>
2020-06-26 13:49:05 +0300

    added id

--- RealEstate.BLL/DTO/UserDtos/UsersListDto.cs
+++ RealEstate.BLL/DTO/UserDtos/UsersListDto.cs
@@ -2,6 +2,7 @@
 {
     public class UsersListDto
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ImagePath { get; set; }
--- RealEstateIdentity/ViewModels/UserViewModels/UsersListViewModel.cs
+++ RealEstateIdentity/ViewModels/UserViewModels/UsersListViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class UsersListViewModel
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ImagePath { get; set; }
