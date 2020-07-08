785c4f2d94e7a2546efacee3de45703d221b5084
Aandriana <abaran@leobit.com>
2020-07-08 05:20:51 +0300

    guard

--- RealEstate/ClientApp/src/app/shared/auth.guard.ts
+++ RealEstate/ClientApp/src/app/shared/auth.guard.ts
@@ -1,54 +1,20 @@
 import { Injectable } from '@angular/core';
 import {
   CanActivate,
-  CanActivateChild,
-  CanDeactivate,
-  CanLoad,
-  Route,
-  UrlSegment,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
   Router
-} from '@angular/router';
-import { Observable } from 'rxjs';
+  } from '@angular/router';
 import {AuthService} from '../core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad {
+export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    let url: string = state.url;
-    return this.checkLogin(url);
-  }
-  checkLogin(url: string): true|UrlTree {
-    if (this.authService.isLoggedIn) { return true; }
-
-    // Store the attempted URL for redirecting
-    this.authService.redirectUrl = url;
-
-    // Redirect to the login page
-    return this.router.parseUrl('/login');
-  }
-  canActivateChild(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canDeactivate(
-    component: unknown,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(): boolean {
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['login']);
+      return false;
+    }
     return true;
   }
 }
--- RealEstate/ClientApp/src/app/user/user.module.ts
+++ RealEstate/ClientApp/src/app/user/user.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UserRoutingModule} from './user-routing.module';
 import { HomeComponent } from './home/home.component';
+import {AuthGuard} from '../shared/auth.guard';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
+  ],
+  providers: [
+      AuthGuard
   ]
 })
+
 export class UserModule { }
